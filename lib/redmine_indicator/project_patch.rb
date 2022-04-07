3240a76c3ef15c68eaa33446c18404cec91d2577
Yongwook Jung <ywjung99@naver.com>
2022-04-07 11:01:20 +0900

    Update project_patch.rb
    Cannot define multiple 'included' blocks for a Concern 문제 해결
    참고 :  https://stackoverflow.com/questions/24541423/multiple-activesupport-concerns-in-mongoid-model

--- lib/redmine_indicator/project_patch.rb
+++ lib/redmine_indicator/project_patch.rb
@@ -1,16 +1,17 @@
 module RedmineIndicator
   module ProjectPatch
-    extend ActiveSupport::Concern
 
     ::AVAILABLE_REPORTS = ['issues_summary','issues_opened_closed_today','issues_opened_closed','issues_opened_closed_10days',
                            'issues_opened_closed_10weeks','spent_time_10days','spent_time_10weeks','issues_by_priority',
                            'issues_by_priority_all','issues_by_status','issues_by_status_all','issues_by_tracker','issues_by_tracker_all']
 
-    included do
-      serialize :indicator_left_top
-      serialize :indicator_left_bottom
-      serialize :indicator_right
-      safe_attributes :indicator_left_top, :indicator_left_bottom, :indicator_right
+    def self.included(klass)
+      klass.class_eval do
+        serialize :indicator_left_top
+        serialize :indicator_left_bottom
+        serialize :indicator_right
+        safe_attributes :indicator_left_top, :indicator_left_bottom, :indicator_right
+      end
     end
 
     def indicator_left_top_empty
